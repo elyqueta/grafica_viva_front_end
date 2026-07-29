0d74fa2a2dbc80a7e1da3d384da6ab827ccb1d0c
csassapa18 <claudio.sassapa@evolium.ao>
2026-07-29 14:23:14 +0100

    feat: replace Hero component with development message in multiple pages

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -1,15 +1,14 @@
-import Navbar from '../components/NavBar';
-import Footer from '../components/Footer';
-import PageShell from '../components/homeComponents/PageShell';
-import Hero from '../components/homeComponents/Hero';
+import Navbar from "../components/NavBar";
+import Footer from "../components/Footer";
+import PageShell from "../components/homeComponents/PageShell";
 
 export default function AboutPage() {
   return (
     <PageShell footer={<Footer />}>
       <Navbar />
-
-      <Hero />
-     
+      <div className="p-20 h-100 text-center text-3xl font-bold">
+        <h1>Página em desenvolvimento</h1>
+      </div>
     </PageShell>
   );
 }
