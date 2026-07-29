781303ec98b100b07105f293db46c688b2c41092
csassapa18 <claudio.sassapa@evolium.ao>
2026-07-29 15:22:01 +0100

    feat: update layout to use Poppins font for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const poppins = Poppins({
   variable: "--font-geist-sans",
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  weight: ["400"],
   subsets: ["latin"],
 });
 
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="pt-PT"
-      className={`${geistSans.variable} ${geistMono.variable} bg-amber-50`}
+      className={`${poppins.variable} ${geistMono.variable} bg-amber-50`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
